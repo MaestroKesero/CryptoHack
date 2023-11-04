7767779f863b3f486c36263d4ff1c8bdc951f0fe
Kesero <90833008+MaestroKesero@users.noreply.github.com>
2023-11-04 18:55:13 +0100

    Add files via upload

---
+++ General/XOR/XOR_starter.py
@@ -0,0 +1,21 @@
+from pwn import xor
+
+def xor_pwn():
+
+    result_pwn = xor(label, number)
+    flag = f"crypto{{{result_pwn.decode('ISO-8859-1')}}}"
+    print("La forma xor() es:",flag)
+
+def xor_manually():
+
+    result_manually_bytes = bytes([a ^ number for a in label])
+    result_manually = result_manually_bytes.decode('ISO-8859-1')
+    flag = f"crypto{{{result_manually}}}"
+    print(f"La forma manual de XOR es: ",flag)
+    
+    
+label = b'label' #Ya esta en bytes, no necesitamos lable.encode('ISO-8859-1')
+number = 13
+
+xor_pwn()
+xor_manually()
