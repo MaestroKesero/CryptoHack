3ab3e88e6e4b41a879362935158b5c90b79b2613
Kesero <90833008+MaestroKesero@users.noreply.github.com>
2023-11-06 12:19:54 +0000

    Add files via upload

---
+++ General/Mathematics/Modular_arithmetic1.py
@@ -0,0 +1,14 @@
+def module(a,n):
+
+    b = a % n
+    print(f"El modulo es: ", b)
+
+a = 11
+n = 6
+module(a,n)
+
+a = 8146798528947 
+n = 17
+module(a,n)
+
+# a ≡ b mod n se interpreta como a / n y el resto es b, por lo que nos piden calcular los restos.
