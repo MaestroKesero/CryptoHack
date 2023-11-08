cdaf1a6ab540d1a9f2f1bf65bde697ed1d36d2d1
Kesero <90833008+MaestroKesero@users.noreply.github.com>
2023-11-08 11:18:34 +0100

    Add files via upload

---
+++ General/Encoding/Hex.py
@@ -0,0 +1,11 @@
+
+def hexToBytes():
+    
+    number_bytes = bytes.fromhex(number_hex)
+    print(number_bytes)
+    
+
+number_hex = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+hexToBytes()
+
+# Esta funcion transforma un numero hexadecimal a bytes
