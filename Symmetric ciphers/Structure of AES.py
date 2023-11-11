9c56ac12b363f68a965c1bebf8c5af3dfe9c65af
Kesero <90833008+MaestroKesero@users.noreply.github.com>
2023-11-11 20:45:31 +0000

    Update Structure of AES.py

--- Symmetric ciphers/Structure of AES.py
+++ Symmetric ciphers/Structure of AES.py
@@ -1,1 +1,19 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
 
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    return bytes([matrix[i][j] for i in range(4) for j in range(4)])
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+
+# Simplemente teniamos que transformar cada valor de la matriz en bytes
+# y posteriormente con [] transformamos dichos bytes a una lista.
