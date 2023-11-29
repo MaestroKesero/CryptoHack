aa16df451183e159ee2bffc100ecf8a6430c2e67
Kesero <90833008+MaestroKesero@users.noreply.github.com>
2023-11-29 12:57:34 +0100

    Update Password as keys.py

--- Symmetric ciphers/Symmetric Starter/Password as keys.py
+++ Symmetric ciphers/Symmetric Starter/Password as keys.py
@@ -1,21 +1,28 @@
 import requests
 
-def findKey(ciphertext_bytex):
-
-  with open('/usr/share/dict/words') as f:
-    for words in f:
-      words = words.strip()
-      key_try_hex = hashlib.md5(word.encode()).digest()
-      key_try_bytes = bytes.fromhex(key_try_hex)
-      print(key_try_bytes)
-    
-  return key_try_bytes
-
+# Obtenemos la flag cifrada
 result = result.get('https://aes.cryptohack.org/passwords_as_keys/encrypt_flag')
 ciphertext = result.json()["ciphertext"]
 print("La flag cifrada es: ", ciphertext)
-ciphertext_bytes = bytes.fromhex(ciphertext_hex)
-key = findKey(ciphertext_bytes)
+ciphertext_bytes = bytes.fromhex(ciphertext)
+
+# Rompemos key
+with open('/usr/share/dict/words.txt','r') as f:
+ for words in f:
+      words = words.strip()
+      key_try_hex = hashlib.md5(word.encode()).digest()
+      key_try_bytes = bytes.fromhex(key_try_hex)
+      #print(key_try_bytes) 
+
+      # Comprobamos posible key
+      result_flag = result_flag.get('/passwords_as_keys/decrypt/{ciphertext_bytes}/{key}/')
+      flag_try = result_flag.json()["plaintext"]
+      print(f"Posible flag: {flag_try}, llave utilizada: {key}")
+
+      # Guardamos posibles flags
+      with open('all_flags.txt', 'w') as flags:
+      structure = f"Posible flag: {flag_try}, llave utilizada: {key}"
+      flags.write(sructure)
 
 
 # La clave para romper este ECB, es que si observamos el codigo, podemos observar que la Key se genera 
@@ -25,8 +32,8 @@ key = findKey(ciphertext_bytes)
 # keyword = random.choice(words)
 #
 # KEY = hashlib.md5(keyword.encode()).digest()
-# Por tanto para romper dicho cifrado, necesitamos obtener la clave, para ello realizaremos el proceso inverso
-# y posteriormente tendremos que realizar la operacion XOR entre el ciphertext y la Key (En este caso al utilziar Crypto.Utils.AES directamente
-# podemos utilizar la funcion decrypt del tiron).
+# Por tanto para romper dicho cifrado, necesitamos la llave que la conseguimos computando todos los valores de key
+# Posteriormente descifraremos con dicha clave y el ciphertexto y por ultimo buscaremos la flag.
+# Podemos buscar la flag mediante un grep del archivo o de forma mas sencilla, utilizando if b'crypto{ in flag_try: print(flag_try)
 
 # https://blog.csdn.net/shshss64/article/details/128029134
